bf0570080ed84656e094df79e4637fa0066b2a24
digiaggregation <72462793+digiaggregation@users.noreply.github.com>
2024-04-28 12:55:38 +0530

    Update iot_project.ino

--- iot_project.ino
+++ iot_project.ino
@@ -18,7 +18,7 @@
 
 Adafruit_INA219 ina219;
 
-//Display 
+//Display Parameters
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
